c57e9c9e278eeaa182c592d9bca305794b61731c
Sandeep-singh-99 <sandeep.necoder@gmail.com>
2025-03-27 16:36:27 +0530

    refactor SideBar component for improved styling and user experience; update background color in CSS

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -15,49 +15,50 @@ import { useSelector } from 'react-redux';
 export default function SideBar() {
   const {  isAuthenticated, user, loading } = useSelector((state) => state.auth)
   return (
-    <div className="fixed top-0 left-10 h-screen w-20 lg:w-64 flex flex-col justify-between border-r border-gray-700">
+    <div className="fixed top-0 lg:left-5 left-0 h-screen w-23 lg:w-64 flex flex-col justify-between lg:items-stretch items-center border-r border-gray-700">
       
-      <div className="flex flex-col items-center lg:items-start">
+      <div className="flex flex-col items-start">
         {/* Logo */}
         <div className="p-4">
-          <h1 className="text-2xl font-bold text-blue-600 lg:text-3xl">
+          <h1 className='text-2xl font-bold md:hidden text-white lg:text-3xl font-serif'>T</h1>
+          <h1 className="text-2xl font-bold hidden md:block text-white lg:text-3xl">
             Tweetz
           </h1>
         </div>
 
         
-        <nav className="space-y-6 mt-4 w-full">
-          <Link to={"/"} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+        <nav className="space-y-4 mt-4 w-full">
+          <Link to={"/"} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <Home className="w-6 h-6" />
-            <span className="hidden lg:inline ml-4 text-lg">Home</span>
+            <span className="hidden lg:inline ml-4 text-xl">Home</span>
           </Link>
 
-          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <Hash className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">Explore</span>
           </Link>
 
-          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <Bell className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">Notifications</span>
           </Link>
 
-          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <Mail className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">Messages</span>
           </Link>
 
-          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <Bookmark className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">Bookmarks</span>
           </Link>
 
-          <Link to={"/profile"} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={"/profile"} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <User className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">Profile</span>
           </Link>
 
-          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-gray-800 rounded-full transition-colors lg:w-11/12">
+          <Link to={""} className="flex items-center px-4 py-2 text-white hover:bg-[#181818] rounded-full transition-colors lg:w-11/12">
             <MoreHorizontal className="w-6 h-6" />
             <span className="hidden lg:inline ml-4 text-lg">More</span>
           </Link>
@@ -74,14 +75,14 @@ export default function SideBar() {
             </div>
           ) :
           isAuthenticated ? (
-            <div className="flex items-center space-x-4 hover:bg-gray-800 rounded-full transition-colors p-2">
+            <div className="flex items-center space-x-4 hover:bg-[#181818] rounded-full transition-colors p-2">
               <img src={user.data.profileImage} alt="Profile" className="w-12 h-12 rounded-full" />
-              <p className='text-xl'>{user.data.fullName}</p>
+              <p className='text-xl hidden md:block'>{user.data.fullName}</p>
             </div>
           ) : (
             <Link to={"/login"} className="w-full flex items-center justify-center lg:justify-start px-4 py-2 bg-blue-600 text-white rounded-full hover:bg-blue-700 transition-colors">
             <LogIn className="w-6 h-6 lg:mr-2" />
-            <span className="hidden lg:inline text-lg">Login</span>
+            <span className="hidden lg:inline text-xl">Login</span>
           </Link>
           )
         }
@@ -89,3 +90,5 @@ export default function SideBar() {
     </div>
   );
 }
+
+
